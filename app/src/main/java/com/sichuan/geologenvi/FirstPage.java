69667179828f3093d18e64c8d672c4699ebd8ca0
xuff78 <xuff78@163.com>
2016-07-19 23:20:18 +0800

    没啥aaaaaa

--- app/src/main/java/com/sichuan/geologenvi/FirstPage.java
+++ app/src/main/java/com/sichuan/geologenvi/FirstPage.java
@@ -20,7 +20,7 @@ public class FirstPage extends AppCompatActivity {
 
     private final int BUFFER_SIZE = 400000;
     private boolean ready=false;
-    private int i=0;
+//    private int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
