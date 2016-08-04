df455cc1268e1384c7a7fa05172f56edf2c3ecaa
xuyichao <xuyichao@infohold.com.cn>
2016-08-04 18:01:03 +0800

    采集信息

--- app/src/main/java/com/sichuan/geologenvi/act/report/BanqianbirangEditMain.java
+++ app/src/main/java/com/sichuan/geologenvi/act/report/BanqianbirangEditMain.java
@@ -1,5 +1,8 @@
 package com.sichuan.geologenvi.act.report;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,6 +15,7 @@ import android.widget.TextView;
 import com.sichuan.geologenvi.DataBase.SqlHandler;
 import com.sichuan.geologenvi.R;
 import com.sichuan.geologenvi.act.AppFrameAct;
+import com.sichuan.geologenvi.act.geodisaster.SelectorAct;
 import com.sichuan.geologenvi.bean.MapBean;
 import com.sichuan.geologenvi.frg.BanqianbirangBase;
 import com.sichuan.geologenvi.frg.BanqianshenqingFrg;
@@ -20,6 +24,13 @@ import com.sichuan.geologenvi.frg.CJ_GCZL_XCKP1;
 import com.sichuan.geologenvi.frg.CJ_GCZL_XCKP2;
 import com.sichuan.geologenvi.frg.CJ_GCZL_XCKP3;
 import com.sichuan.geologenvi.frg.FormInfoFrg;
+import com.sichuan.geologenvi.http.CallBack;
+import com.sichuan.geologenvi.http.HttpHandler;
+import com.sichuan.geologenvi.utils.DialogUtil;
+import com.sichuan.geologenvi.utils.JsonUtil;
+import com.sichuan.geologenvi.utils.ToastUtils;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -37,7 +48,24 @@ public class BanqianbirangEditMain extends AppFrameAct {
     private SqlHandler handler;
     private TextView updateDataBtn, delDataBtn, addDataBtn;
     private Map<String, String> infoMap=new HashMap<>();
+    private CJ_GCZL_XCKP1 frg1;
+    private CJ_GCZL_XCKP2 frg2;
+    private CJ_GCZL_XCKP3 frg3;
+    private HttpHandler httpHandler;
+    private String requesType="";
+    private String Update="update", Delete="delete", Add="add";
 
+    private void initHandler() {
+        httpHandler=new HttpHandler(this, new CallBack(BanqianbirangEditMain.this){
+
+            @Override
+            public void doSuccess(String method, String jsonData) {
+                ToastUtils.displayTextShort(BanqianbirangEditMain.this, "操作成功");
+                setResult(0x99);
+                finish();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,14 +84,21 @@ public class BanqianbirangEditMain extends AppFrameAct {
             delDataBtn.setVisibility(View.GONE);
         }
         _setHeaderTitle("成都市重大地质灾害防治工程项目现场检查记录表");
+        initHandler();
         handler=new SqlHandler(this);
         initView();
     }
 
     private void initView() {
+        frg1=new CJ_GCZL_XCKP1();
+        frg2=new CJ_GCZL_XCKP2();
+        frg3=new CJ_GCZL_XCKP3();
         updateDataBtn=(TextView) findViewById(R.id.updateDataBtn);
+        updateDataBtn.setOnClickListener(listener);
         delDataBtn=(TextView) findViewById(R.id.delDataBtn);
+        delDataBtn.setOnClickListener(listener);
         addDataBtn=(TextView) findViewById(R.id.addDataBtn);
+        addDataBtn.setOnClickListener(listener);
         mIndicator = (PagerTabStrip) findViewById(R.id.pagertab);
         mIndicator.setTabIndicatorColorResource(R.color.normal_gray);
         mIndicator.setBackgroundResource(R.color.whitesmoke);
@@ -72,6 +107,33 @@ public class BanqianbirangEditMain extends AppFrameAct {
         mViewPager.setAdapter(mAdapter);
     }
 
+    private View.OnClickListener listener=new View.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.addDataBtn:
+                    if(frg1.canbeCreate()){
+                        JSONObject jsonObj=new JSONObject();
+                        frg1.getDataByJson(jsonObj);
+                        requesType=Add;
+                        httpHandler.addCJ_BXCS_XCKP(jsonObj.toString());
+                    }
+                    break;
+                case R.id.updateDataBtn:
+                    break;
+                case R.id.delDataBtn:
+                    DialogUtil.showActionDialog(BanqianbirangEditMain.this, "提示", "确认要删除", new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            requesType=Delete;
+                            httpHandler.delCJ_GCZL_XCKP(infoMap.get("ID"));
+                        }
+                    });
+                    break;
+            }
+        }
+    };
 
     public class TabAdapter extends FragmentPagerAdapter
     {
@@ -93,13 +155,13 @@ public class BanqianbirangEditMain extends AppFrameAct {
             Bundle b=new Bundle();
             switch (arg0){
                 case 0:
-                    curfragment=new CJ_GCZL_XCKP1();
+                    curfragment=frg1;
                     break;
                 case 1:
-                    curfragment=new CJ_GCZL_XCKP2();
+                    curfragment=frg2;
                     break;
                 case 2:
-                    curfragment=new CJ_GCZL_XCKP3();
+                    curfragment=frg3;
                     break;
             }
             curfragment.setArguments(b);
--- app/src/main/java/com/sichuan/geologenvi/frg/CJ_GCZL_XCKP1.java
+++ app/src/main/java/com/sichuan/geologenvi/frg/CJ_GCZL_XCKP1.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.sichuan.geologenvi.DataBase.SqlHandler;
 import com.sichuan.geologenvi.R;
 import com.sichuan.geologenvi.act.geodisaster.SelectorAct;
 import com.sichuan.geologenvi.act.geodisaster.TitleResultListAct;
@@ -22,7 +23,10 @@ import com.sichuan.geologenvi.bean.MapBean;
 import com.sichuan.geologenvi.http.HttpHandler;
 import com.sichuan.geologenvi.utils.ActUtil;
 import com.sichuan.geologenvi.utils.DialogUtil;
+import com.sichuan.geologenvi.utils.JsonUtil;
 import com.sichuan.geologenvi.utils.ToastUtils;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,16 +38,17 @@ public class CJ_GCZL_XCKP1 extends BaseFragment{
 
     DatePickerDialog datePickerDialog;
     private EditText nameEdt, idEdt;
-    private HttpHandler handler;
     private View arrowRight;
     private TextView xgrydgcsxcd, kgrq, jhwgrq, tbrq, projectName, sgdw, jldw, sjdw, dlwz,
             lon1,lon2,lon3,lat1,lat2,lat3;
     private ImageView sfczwffbzb_yes, sfczwffbzb_no,dwzz_sgdw_yes, dwzz_sgdw_no, dwzz_jldw_yes, dwzz_jldw_no, xmjlsfyxyjszc_yes, xmjlsfyxyjszc_no, jlryzgzs_yes, jlryzgzs_no,
             tsgzry_yes, tsgzry_no, sfszwpyt_yes, sfszwpyt_no, zjysfdw_yes, zjysfdw_no, sfyzlkzwj_yes, sfyzlkzwj_no;
-    private EditText gcjzqk, qt, jcfzr, tbr;
-    private String guid="", lon="", lat="";
+    private EditText gcjzqk, qt, jcfzr, tbr, hbz, zbx, zby;
+    private String guid="", lon="", lat="", sfczwffbzb="",dwzz_sgdw="",dwzz_jldw="", xmjlsfyxyjszc="",jlryzgzs="",tsgzry="",
+            sfszwpyt="", zjysfdw="", sfyzlkzwj="";
     private int dialogtype=-1;
     private Map<String, String> infoMap=new HashMap<>();
+    private SqlHandler handler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -51,49 +56,185 @@ public class CJ_GCZL_XCKP1 extends BaseFragment{
 
 
         initView(view);
+        handler=new SqlHandler(getActivity());
         if(getActivity().getIntent().hasExtra("InfoMap")) {
             infoMap=((MapBean)(getActivity().getIntent().getSerializableExtra("InfoMap"))).getMap();
             arrowRight.setVisibility(View.INVISIBLE);
+            initData();
         }else{
             view.findViewById(R.id.projectNameLayout).setOnClickListener(listener);
         }
         return view;
     }
 
+    private void initData() {
+        gcjzqk.setText(infoMap.get("gcjzqk".toUpperCase()));
+        qt.setText(infoMap.get("qt".toUpperCase()));
+        jcfzr.setText(infoMap.get("jcfzr".toUpperCase()));
+        tbr.setText(infoMap.get("tbr".toUpperCase()));
+        hbz.setText(infoMap.get("gczl_z".toUpperCase()));
+        zbx.setText(infoMap.get("gczl_x".toUpperCase()));
+        zby.setText(infoMap.get("gczl_y".toUpperCase()));
+
+        xgrydgcsxcd.setText(infoMap.get("xgrydgcsxcd".toUpperCase()));
+        kgrq.setText(ActUtil.getFormatDate(infoMap.get("kgrq".toUpperCase())));
+        jhwgrq.setText(ActUtil.getFormatDate(infoMap.get("jhwgrq".toUpperCase())));
+        tbrq.setText(ActUtil.getFormatDate(infoMap.get("tbrq".toUpperCase())));
+        projectName.setText(infoMap.get("gczl_name".toUpperCase()));
+        sgdw.setText(infoMap.get("xgrydgcsxcd".toUpperCase()));
+        jldw.setText(infoMap.get("gczl_jldw".toUpperCase()));
+        sjdw.setText(infoMap.get("gczl_sjdw".toUpperCase()));
+        dlwz.setText(infoMap.get("gczl_quxian".toUpperCase())+"  "+infoMap.get("gczl_xiangzhen".toUpperCase())
+                +"  "+infoMap.get("gczl_cun".toUpperCase())+"  "+infoMap.get("gczl_zu".toUpperCase()));
+
+        setLoaction(infoMap.get("gczl_lon".toUpperCase()), infoMap.get("gczl_lat".toUpperCase()));
+
+        sfczwffbzb=infoMap.get("sfczwffbzb".toUpperCase());
+        if(sfczwffbzb.equals("是")) {
+            sfczwffbzb_yes.setImageResource(R.mipmap.app_login_remember_sel);
+        }else if(sfczwffbzb.equals("否")) {
+            sfczwffbzb_no.setImageResource(R.mipmap.app_login_remember_sel);
+        }
+
+        dwzz_sgdw=infoMap.get("dwzz_sgdw".toUpperCase());
+        if(dwzz_sgdw.equals("有")) {
+            dwzz_sgdw_yes.setImageResource(R.mipmap.app_login_remember_sel);
+        }else if(sfczwffbzb.equals("无")) {
+            dwzz_sgdw_no.setImageResource(R.mipmap.app_login_remember_sel);
+        }
+
+        dwzz_jldw=infoMap.get("dwzz_jldw".toUpperCase());
+        if(dwzz_jldw.equals("有")) {
+            dwzz_jldw_yes.setImageResource(R.mipmap.app_login_remember_sel);
+        }else if(dwzz_jldw.equals("无")) {
+            dwzz_jldw_no.setImageResource(R.mipmap.app_login_remember_sel);
+        }
+
+        xmjlsfyxyjszc=infoMap.get("xmjlsfyxyjszc".toUpperCase());
+        if(xmjlsfyxyjszc.equals("有")) {
+            xmjlsfyxyjszc_yes.setImageResource(R.mipmap.app_login_remember_sel);
+        }else if(xmjlsfyxyjszc.equals("无")) {
+            xmjlsfyxyjszc_no.setImageResource(R.mipmap.app_login_remember_sel);
+        }
+
+        jlryzgzs=infoMap.get("jlryzgzs".toUpperCase());
+        if(jlryzgzs.equals("有")) {
+            jlryzgzs_yes.setImageResource(R.mipmap.app_login_remember_sel);
+        }else if(jlryzgzs.equals("无")) {
+            jlryzgzs_no.setImageResource(R.mipmap.app_login_remember_sel);
+        }
+
+        sfszwpyt=infoMap.get("sfszwpyt".toUpperCase());
+        if(sfszwpyt.equals("有")) {
+            sfszwpyt_yes.setImageResource(R.mipmap.app_login_remember_sel);
+        }else if(sfszwpyt.equals("无")) {
+            sfszwpyt_no.setImageResource(R.mipmap.app_login_remember_sel);
+        }
+
+        zjysfdw=infoMap.get("zjysfdw".toUpperCase());
+        if(zjysfdw.equals("是")) {
+            zjysfdw_yes.setImageResource(R.mipmap.app_login_remember_sel);
+        }else if(zjysfdw.equals("否")) {
+            zjysfdw_no.setImageResource(R.mipmap.app_login_remember_sel);
+        }
+
+        tsgzry=infoMap.get("tsgzry".toUpperCase());
+        if(tsgzry.equals("是")) {
+            tsgzry_yes.setImageResource(R.mipmap.app_login_remember_sel);
+        }else if(tsgzry.equals("否")) {
+            tsgzry_no.setImageResource(R.mipmap.app_login_remember_sel);
+        }
+
+        sfyzlkzwj=infoMap.get("sfyzlkzwj".toUpperCase());
+        if(sfyzlkzwj.equals("是")) {
+            sfyzlkzwj_yes.setImageResource(R.mipmap.app_login_remember_sel);
+        }else if(sfyzlkzwj.equals("否")) {
+            sfyzlkzwj_no.setImageResource(R.mipmap.app_login_remember_sel);
+        }
+
+    }
+
+    public void getDataByJson(JSONObject jsonObj){
+
+        JsonUtil.addJsonData(jsonObj, "gczl_guid", guid);
+        JsonUtil.addJsonData(jsonObj, "gcjzqk", gcjzqk.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "qt", qt.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "jcfzr", jcfzr.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "tbr", tbr.getText().toString());
+
+        JsonUtil.addJsonData(jsonObj, "xgrydgcsxcd", xgrydgcsxcd.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "kgrq", kgrq.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "jhwgrq", jhwgrq.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "tbrq", tbrq.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "gczl_name", projectName.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "gczl_sjdw", sjdw.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "gczl_jldw", jldw.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "gczl_sgdw", sgdw.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "gczl_lon", lon);
+        JsonUtil.addJsonData(jsonObj, "gczl_lat", lat);
+        JsonUtil.addJsonData(jsonObj, "gczl_quxian", gczl_quxian);
+        JsonUtil.addJsonData(jsonObj, "gczl_xiangzhen", gczl_xiangzhen);
+        JsonUtil.addJsonData(jsonObj, "gczl_cun", gczl_cun);
+        JsonUtil.addJsonData(jsonObj, "gczl_zu", gczl_zu);
+        JsonUtil.addJsonData(jsonObj, "gczl_x", zbx.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "gczl_y", zby.getText().toString());
+        JsonUtil.addJsonData(jsonObj, "gczl_z", hbz.getText().toString());
+
+        JsonUtil.addJsonData(jsonObj, "sfczwffbzb", sfczwffbzb);
+        JsonUtil.addJsonData(jsonObj, "dwzz_sgdw", dwzz_sgdw);
+        JsonUtil.addJsonData(jsonObj, "dwzz_jldw", dwzz_jldw);
+        JsonUtil.addJsonData(jsonObj, "xmjlsfyxyjszc", xmjlsfyxyjszc);
+        JsonUtil.addJsonData(jsonObj, "jlryzgzs", jlryzgzs);
+        JsonUtil.addJsonData(jsonObj, "sfszwpyt", sfszwpyt);
+        JsonUtil.addJsonData(jsonObj, "zjysfdw", zjysfdw);
+        JsonUtil.addJsonData(jsonObj, "sfyzlkzwj", sfyzlkzwj);
+        JsonUtil.addJsonData(jsonObj, "tsgzry", tsgzry);
+
+    }
+
     private void initView(View v) {
         arrowRight=v.findViewById(R.id.arrowRight);
+
         sfczwffbzb_yes= (ImageView) v.findViewById(R.id.sfczwffbzb_yes);
         sfczwffbzb_yes.setOnClickListener(listener);
         sfczwffbzb_no= (ImageView) v.findViewById(R.id.sfczwffbzb_no);
         sfczwffbzb_no.setOnClickListener(listener);
+
         dwzz_sgdw_yes= (ImageView) v.findViewById(R.id.dwzz_sgdw_yes);
         dwzz_sgdw_yes.setOnClickListener(listener);
         dwzz_sgdw_no= (ImageView) v.findViewById(R.id.dwzz_sgdw_no);
         dwzz_sgdw_no.setOnClickListener(listener);
+
         dwzz_jldw_yes= (ImageView) v.findViewById(R.id.dwzz_jldw_yes);
         dwzz_jldw_yes.setOnClickListener(listener);
         dwzz_jldw_no= (ImageView) v.findViewById(R.id.dwzz_jldw_no);
         dwzz_jldw_no.setOnClickListener(listener);
+
         xmjlsfyxyjszc_yes= (ImageView) v.findViewById(R.id.xmjlsfyxyjszc_yes);
         xmjlsfyxyjszc_yes.setOnClickListener(listener);
         xmjlsfyxyjszc_no= (ImageView) v.findViewById(R.id.xmjlsfyxyjszc_no);
         xmjlsfyxyjszc_no.setOnClickListener(listener);
+
         jlryzgzs_yes= (ImageView) v.findViewById(R.id.jlryzgzs_yes);
         jlryzgzs_yes.setOnClickListener(listener);
         jlryzgzs_no= (ImageView) v.findViewById(R.id.jlryzgzs_no);
         jlryzgzs_no.setOnClickListener(listener);
+
         tsgzry_yes= (ImageView) v.findViewById(R.id.tsgzry_yes);
         tsgzry_yes.setOnClickListener(listener);
         tsgzry_no= (ImageView) v.findViewById(R.id.tsgzry_no);
         tsgzry_no.setOnClickListener(listener);
+
         sfszwpyt_yes= (ImageView) v.findViewById(R.id.sfszwpyt_yes);
         sfszwpyt_yes.setOnClickListener(listener);
         sfszwpyt_no= (ImageView) v.findViewById(R.id.sfszwpyt_no);
         sfszwpyt_no.setOnClickListener(listener);
+
         zjysfdw_yes= (ImageView) v.findViewById(R.id.zjysfdw_yes);
         zjysfdw_yes.setOnClickListener(listener);
         zjysfdw_no= (ImageView) v.findViewById(R.id.zjysfdw_no);
         zjysfdw_no.setOnClickListener(listener);
+
         sfyzlkzwj_yes= (ImageView) v.findViewById(R.id.sfyzlkzwj_yes);
         sfyzlkzwj_yes.setOnClickListener(listener);
         sfyzlkzwj_no= (ImageView) v.findViewById(R.id.sfyzlkzwj_no);
@@ -120,6 +261,9 @@ public class CJ_GCZL_XCKP1 extends BaseFragment{
         qt= (EditText) v.findViewById(R.id.qt);
         jcfzr= (EditText) v.findViewById(R.id.jcfzr);
         tbr= (EditText) v.findViewById(R.id.tbr);
+        hbz= (EditText) v.findViewById(R.id.hbz);
+        zbx= (EditText) v.findViewById(R.id.zbx);
+        zby= (EditText) v.findViewById(R.id.zby);
 
         v.findViewById(R.id.xgrydgcsxcdLayout).setOnClickListener(listener);
         v.findViewById(R.id.kgrqLayout).setOnClickListener(listener);
@@ -173,71 +317,96 @@ public class CJ_GCZL_XCKP1 extends BaseFragment{
                     });
                     break;
                 case R.id.sfczwffbzb_yes:
+                    sfczwffbzb="是";
                     sfczwffbzb_yes.setImageResource(R.mipmap.app_login_remember_sel);
                     sfczwffbzb_no.setImageResource(R.mipmap.app_login_remember_unsel);
                     break;
                 case R.id.sfczwffbzb_no:
+                    sfczwffbzb="否";
                     sfczwffbzb_yes.setImageResource(R.mipmap.app_login_remember_unsel);
                     sfczwffbzb_no.setImageResource(R.mipmap.app_login_remember_sel);
                     break;
+                case R.id.dwzz_sgdw_yes:
+                    dwzz_sgdw="有";
+                    dwzz_sgdw_yes.setImageResource(R.mipmap.app_login_remember_sel);
+                    dwzz_sgdw_no.setImageResource(R.mipmap.app_login_remember_unsel);
+                    break;
+                case R.id.dwzz_sgdw_no:
+                    dwzz_sgdw="无";
+                    dwzz_sgdw_yes.setImageResource(R.mipmap.app_login_remember_unsel);
+                    dwzz_sgdw_no.setImageResource(R.mipmap.app_login_remember_sel);
+                    break;
                 case R.id.dwzz_jldw_yes:
+                    dwzz_jldw="有";
                     dwzz_jldw_yes.setImageResource(R.mipmap.app_login_remember_sel);
                     dwzz_jldw_no.setImageResource(R.mipmap.app_login_remember_unsel);
                     break;
                 case R.id.dwzz_jldw_no:
+                    dwzz_jldw="无";
                     dwzz_jldw_yes.setImageResource(R.mipmap.app_login_remember_unsel);
                     dwzz_jldw_no.setImageResource(R.mipmap.app_login_remember_sel);
                     break;
                 case R.id.xmjlsfyxyjszc_yes:
+                    xmjlsfyxyjszc="有";
                     xmjlsfyxyjszc_yes.setImageResource(R.mipmap.app_login_remember_sel);
                     xmjlsfyxyjszc_no.setImageResource(R.mipmap.app_login_remember_unsel);
                     break;
                 case R.id.xmjlsfyxyjszc_no:
+                    xmjlsfyxyjszc="无";
                     xmjlsfyxyjszc_yes.setImageResource(R.mipmap.app_login_remember_unsel);
                     xmjlsfyxyjszc_no.setImageResource(R.mipmap.app_login_remember_sel);
                     break;
                 case R.id.jlryzgzs_yes:
+                    jlryzgzs="有";
                     jlryzgzs_yes.setImageResource(R.mipmap.app_login_remember_sel);
                     jlryzgzs_no.setImageResource(R.mipmap.app_login_remember_unsel);
                     break;
                 case R.id.jlryzgzs_no:
+                    jlryzgzs="无";
                     jlryzgzs_yes.setImageResource(R.mipmap.app_login_remember_unsel);
                     jlryzgzs_no.setImageResource(R.mipmap.app_login_remember_sel);
                     break;
                 case R.id.tsgzry_yes:
+                    tsgzry="有";
                     tsgzry_yes.setImageResource(R.mipmap.app_login_remember_sel);
                     tsgzry_no.setImageResource(R.mipmap.app_login_remember_unsel);
                     break;
                 case R.id.tsgzry_no:
+                    tsgzry="无";
                     tsgzry_yes.setImageResource(R.mipmap.app_login_remember_unsel);
                     tsgzry_no.setImageResource(R.mipmap.app_login_remember_sel);
                     break;
                 case R.id.sfszwpyt_yes:
+                    sfszwpyt="是";
                     sfszwpyt_yes.setImageResource(R.mipmap.app_login_remember_sel);
                     sfszwpyt_no.setImageResource(R.mipmap.app_login_remember_unsel);
                     break;
                 case R.id.sfszwpyt_no:
+                    sfszwpyt="否";
                     sfszwpyt_yes.setImageResource(R.mipmap.app_login_remember_unsel);
                     sfszwpyt_no.setImageResource(R.mipmap.app_login_remember_sel);
                     break;
                 case R.id.zjysfdw_yes:
+                    zjysfdw="是";
                     zjysfdw_yes.setImageResource(R.mipmap.app_login_remember_sel);
                     zjysfdw_no.setImageResource(R.mipmap.app_login_remember_unsel);
                     break;
                 case R.id.zjysfdw_no:
+                    zjysfdw="否";
                     zjysfdw_yes.setImageResource(R.mipmap.app_login_remember_unsel);
                     zjysfdw_no.setImageResource(R.mipmap.app_login_remember_sel);
                     break;
                 case R.id.sfyzlkzwj_yes:
+                    sfyzlkzwj="是";
                     sfyzlkzwj_yes.setImageResource(R.mipmap.app_login_remember_sel);
                     sfyzlkzwj_no.setImageResource(R.mipmap.app_login_remember_unsel);
                     break;
                 case R.id.sfyzlkzwj_no:
+                    sfyzlkzwj="否";
                     sfyzlkzwj_yes.setImageResource(R.mipmap.app_login_remember_unsel);
                     sfyzlkzwj_no.setImageResource(R.mipmap.app_login_remember_sel);
                     break;
                 case R.id.projectNameLayout:
-
                     Intent zdmcIntent=new Intent(getActivity(), SelectorAct.class);
                     zdmcIntent.putExtra("Report", true);
                     zdmcIntent.putExtra("Type", 3);
@@ -274,6 +443,8 @@ public class CJ_GCZL_XCKP1 extends BaseFragment{
         }
     };
 
+    private String gczl_quxian="", gczl_xiangzhen="", gczl_cun="", gczl_zu="";
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 0x11 && resultCode == 0x10) {
@@ -296,7 +467,16 @@ public class CJ_GCZL_XCKP1 extends BaseFragment{
             sgdw.setText(mapBean.getMap().get("ZHCA01A230"));
             jldw.setText(mapBean.getMap().get("ZHCA01A210"));
             sjdw.setText(mapBean.getMap().get("ZHCA01A200"));
-            dlwz.setText(mapBean.getMap().get("ZHCA01A070"));
+
+            gczl_quxian=mapBean.getMap().get("ZHCA01A040");
+            if(gczl_quxian!=null)
+                gczl_quxian=handler.getDistrictName(gczl_quxian);
+            gczl_xiangzhen=mapBean.getMap().get("ZHCA01A050");
+            if(gczl_xiangzhen!=null)
+                gczl_xiangzhen=handler.getDistrictName(gczl_xiangzhen);
+            gczl_cun=mapBean.getMap().get("ZHCA01A060");
+            gczl_zu=mapBean.getMap().get("ZHCA01A070");
+            dlwz.setText(gczl_quxian+"  "+gczl_xiangzhen+"  "+gczl_cun+"  "+gczl_zu);
 //            hb.setText(mapBean.getMap().get("ZHCA01A210"));
 
             String lon = mapBean.getMap().get("ZHCA01A075");
