738605ac617dcc2920fe9fd2c4473af3e76b31e3
cuikailei <cuikailei2006@163.com>
2016-09-02 22:03:06 +0800

    修改，  崔凯磊

--- app/src/main/java/com/sichuan/geologenvi/act/DxsAct.java
+++ app/src/main/java/com/sichuan/geologenvi/act/DxsAct.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.widget.SectionIndexer;
 import android.widget.TextView;
 
+import com.sichuan.geologenvi.DataBase.SqlHandler;
 import com.sichuan.geologenvi.R;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 
 /**
@@ -15,6 +19,9 @@ public class DxsAct   extends AppFrameAct {
     private TextView content;
 
     private int type=0;
+    private SqlHandler handler;
+    ArrayList<Map<String, String>> datalist=new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +32,18 @@ public class DxsAct   extends AppFrameAct {
 
 
         initView();
+
+        handler=new SqlHandler(this);
+        datalist=handler.getQueryResult("SL_TBLDXS", "");
+        String neirong="";
          switch (type) {
            case 0:
-
-                content.setText("成都市红层区面积为4075.42km2，占全市面积近1/3，覆盖11个区（市）县的94个乡（镇）,2014年,我站利用在用红层水井建立了3口监测井，2015年，通过18口红层地下水监测井的建设，初步形成成都市红层地下水监测网络,2016年继续新增了10口红层地下水监测井，加密了监测区域。");
+                neirong=datalist.get(0).get("jbgk");
+                content.setText(neirong);
                 break;
             case 1:
-
-                content.setText("根据目前监测情况，红层地下水类型主要为浅层红层风化裂隙水，水量、水质分布不均匀，整体情况良好。监测井点的布设在满足基本监测目的同时，点优先布设在易于维护、交通便利的地方，便于后期的维护和管理。");
+                neirong=datalist.get(0).get("jcqk");
+                content.setText(neirong);
                 break;
         }
     }
